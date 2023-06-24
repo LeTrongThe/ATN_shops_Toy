255781373dbc643bc212a53e035add2008881774
LeTrongThe <121842902+LeTrongThe@users.noreply.github.com>
2023-06-25 00:57:18 +0700

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,6 +1,6 @@
 <?php 
-    $conn = mysqli_connect('localhost', 'root', '', 'atndb')
-    or die("can not connect database".mysqli_connect());
+       $conn = mysqli_connect('us-cdbr-east-06.cleardb.net','b40c06663c95b9','e9ab330c','heroku_687437c2f670162') 
+    or die("Cannot connect database".mysqli_connect_error());
 
    
 
