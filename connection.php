48e8a00ab7456b7c1794299599ab23bbfe5c265e
LeTrongThe <theltgcc200111@fpt.edu.vn>
2023-06-25 01:07:39 +0700

    the

--- connection.php
+++ connection.php
@@ -2,7 +2,6 @@
        $conn = mysqli_connect('us-cdbr-east-06.cleardb.net','b40c06663c95b9','e9ab330c','heroku_161f39da18d2475') 
     or die("Cannot connect database".mysqli_connect_error());
 
-   
 
     // mysqli_query($conn, 'SET NAMES "utf8"')
 ?>
